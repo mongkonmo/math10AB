d7cee6697e3f27da9b668234c30f3ec56f4433f7
Mong Kon Mo <85360410+mongkonmo@users.noreply.github.com>
2023-05-18 15:33:06 +0000

    Latest build of target web.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -16,16 +16,16 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "1.1",
   "title": "Introduction to Counting",
-  "body": " Introduction to Counting   It may seem silly at first to have an entire chapter dedicated to the topic of counting at this stage of your mathematical development. After all, how hard could it be?    Let's consider a few \"counting problems\" to get a better sense of what may actually be involved in this seemingly simple task of counting.   How many squares are in the figure below?   An arrangement of squares of various sizes.       We can enumerate the individual squares as shown below and see that there are five squares.   Enumerating the five squares in Figure         How many dots are in the figure below?   An arrangement of dots.       subitize  While we could also approach this question by enumerating the individual dots, another approach uses a concept called subitizing . This particular arrangement of dots is common and familiar to anyone who has played games that involve rolling a die. So we recognize this particular arrangement of dots and associate it with prior knowledge that there are five dots.   Tommy has 4 pants and 6 shirts. How many different outfits can he make from these?  Since an outfit consists of a pants and a shirt, we need to count the number of ways we can pair each pants with each shirt. This gives us possible outfits.    As we can see from these problems, there are various counting strategies that can be applied depending on the situation. This chapter will introduce four fundamental strategies for counting:   The Addition Principole of Counting  The Multiplication Principle of Counting  The Rearrangement Principle of Counting  The Labelling Principle of Counting     "
+  "body": " Introduction to Counting   It may seem silly at first to have an entire chapter dedicated to the topic of counting at this stage of your mathematical development. After all, how hard could it be?    Let's consider a few \"counting problems\" to get a better sense of what may actually be involved in this seemingly simple task of counting.   How many squares are in the figure below?   An arrangement of squares of various sizes.       We can enumerate the individual squares as shown below and see that there are five squares.   Enumerating the five squares in Figure         How many dots are in the figure below?   An arrangement of dots.       subitize  While we could also approach this question by enumerating the individual dots, another approach uses a concept called subitizing . This particular arrangement of dots is common and familiar to anyone who has played games that involve rolling a die. So we recognize this particular arrangement of dots and associate it with prior knowledge that there are five dots.   Tommy has 4 pants and 6 shirts. How many different outfits can he make from these?  Since an outfit consists of a pants and a shirt, we need to count the number of ways we can pair each pants with each shirt. This gives us possible outfits.    As we can see from these solutions, there are various counting strategies that can be applied depending on the situation. This chapter will introduce four fundamental strategies for counting:   The Addition Principle of Counting  The Multiplication Principle of Counting  The Rearrangement Principle of Counting  The Labelling Principle of Counting     "
 },
 {
-  "id": "investigation-1",
+  "id": "inv-counting",
   "level": "2",
-  "url": "sec-counting-intro.html#investigation-1",
+  "url": "sec-counting-intro.html#inv-counting",
   "type": "Investigation",
   "number": "1.1.1",
   "title": "",
-  "body": " Let's consider a few \"counting problems\" to get a better sense of what may actually be involved in this seemingly simple task of counting.   How many squares are in the figure below?   An arrangement of squares of various sizes.       We can enumerate the individual squares as shown below and see that there are five squares.   Enumerating the five squares in Figure         How many dots are in the figure below?   An arrangement of dots.       subitize  While we could also approach this question by enumerating the individual dots, another approach uses a concept called subitizing . This particular arrangement of dots is common and familiar to anyone who has played games that involve rolling a die. So we recognize this particular arrangement of dots and associate it with prior knowledge that there are five dots.   Tommy has 4 pants and 6 shirts. How many different outfits can he make from these?  Since an outfit consists of a pants and a shirt, we need to count the number of ways we can pair each pants with each shirt. This gives us possible outfits.    As we can see from these problems, there are various counting strategies that can be applied depending on the situation. This chapter will introduce four fundamental strategies for counting:   The Addition Principole of Counting  The Multiplication Principle of Counting  The Rearrangement Principle of Counting  The Labelling Principle of Counting    "
+  "body": " Let's consider a few \"counting problems\" to get a better sense of what may actually be involved in this seemingly simple task of counting.   How many squares are in the figure below?   An arrangement of squares of various sizes.       We can enumerate the individual squares as shown below and see that there are five squares.   Enumerating the five squares in Figure         How many dots are in the figure below?   An arrangement of dots.       subitize  While we could also approach this question by enumerating the individual dots, another approach uses a concept called subitizing . This particular arrangement of dots is common and familiar to anyone who has played games that involve rolling a die. So we recognize this particular arrangement of dots and associate it with prior knowledge that there are five dots.   Tommy has 4 pants and 6 shirts. How many different outfits can he make from these?  Since an outfit consists of a pants and a shirt, we need to count the number of ways we can pair each pants with each shirt. This gives us possible outfits.    As we can see from these solutions, there are various counting strategies that can be applied depending on the situation. This chapter will introduce four fundamental strategies for counting:   The Addition Principle of Counting  The Multiplication Principle of Counting  The Rearrangement Principle of Counting  The Labelling Principle of Counting    "
 },
 {
   "id": "sec-counting-add-multiply",
@@ -34,7 +34,7 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "1.2",
   "title": "The Addition and Multiplication Principles of Counting",
-  "body": " The Addition and Multiplication Principles of Counting    Understand when and how to use the Addition Principle of Counting  Understand when and how to use the Multiplication Principle of Counting    "
+  "body": " The Addition and Multiplication Principles of Counting    Understand when and how to use the Addition Principle of Counting  Understand when and how to use the Multiplication Principle of Counting      Suppose that a restaurant menu has 6 appetizers and 10 entrées. How many options do you have if  you only order one dish, either an appetizer or an entrée?  you are hungry and will order both an appetizer and an entrée?      "
 },
 {
   "id": "objectives-1",
@@ -46,13 +46,31 @@ var ptx_lunr_docs = [
   "body": "  Understand when and how to use the Addition Principle of Counting  Understand when and how to use the Multiplication Principle of Counting   "
 },
 {
+  "id": "exp-add-multiply",
+  "level": "2",
+  "url": "sec-counting-add-multiply.html#exp-add-multiply",
+  "type": "Exploration",
+  "number": "1.2.1",
+  "title": "",
+  "body": "  Suppose that a restaurant menu has 6 appetizers and 10 entrées. How many options do you have if  you only order one dish, either an appetizer or an entrée?  you are hungry and will order both an appetizer and an entrée?     "
+},
+{
   "id": "sec-counting-rearrangement",
   "level": "1",
   "url": "sec-counting-rearrangement.html",
   "type": "Section",
   "number": "1.3",
   "title": "The Rearrangement Principle of Counting",
-  "body": " The Rearrangement Principle of Counting     Understand when and how to use the Rearrangement Principle of Counting    "
+  "body": " The Rearrangement Principle of Counting  Below is a list of basic skills that will be needed in this section.    Simplifying fractions  Working with factorials      Understand when and how to use the Rearrangement Principle of Counting    "
+},
+{
+  "id": "outcomes-1",
+  "level": "2",
+  "url": "sec-counting-rearrangement.html#outcomes-1",
+  "type": "Prerequisites",
+  "number": "1.3",
+  "title": "",
+  "body": "  Simplifying fractions  Working with factorials   "
 },
 {
   "id": "objectives-2",
@@ -161,6 +179,213 @@ var ptx_lunr_docs = [
   "number": "3.4",
   "title": "Summarizing Data",
   "body": " Summarizing Data   An introduction   "
+},
+{
+  "id": "sec-geometry-intro",
+  "level": "1",
+  "url": "sec-geometry-intro.html",
+  "type": "Section",
+  "number": "4.1",
+  "title": "Introduction to Geometry",
+  "body": " Introduction to Geometry   An introduction   "
+},
+{
+  "id": "sec-geometry-constructions",
+  "level": "1",
+  "url": "sec-geometry-constructions.html",
+  "type": "Section",
+  "number": "4.2",
+  "title": "Geometric Constructions",
+  "body": " Geometric Constructions   An introduction   "
+},
+{
+  "id": "sec-geometry-angle-pair-relations",
+  "level": "1",
+  "url": "sec-geometry-angle-pair-relations.html",
+  "type": "Section",
+  "number": "4.3",
+  "title": "Angle Pair Relationships",
+  "body": " Angle Pair Relationships   An introduction   "
+},
+{
+  "id": "sec-geometry-transversals",
+  "level": "1",
+  "url": "sec-geometry-transversals.html",
+  "type": "Section",
+  "number": "4.4",
+  "title": "Parralel Lines and Transversals",
+  "body": " Parralel Lines and Transversals   An introduction   "
+},
+{
+  "id": "sec-geometry-polygon-angles",
+  "level": "1",
+  "url": "sec-geometry-polygon-angles.html",
+  "type": "Section",
+  "number": "4.5",
+  "title": "Angle Relationships in Polygons",
+  "body": " Angle Relationships in Polygons   An introduction   "
+},
+{
+  "id": "sec-geometry-triangles-quads",
+  "level": "1",
+  "url": "sec-geometry-triangles-quads.html",
+  "type": "Section",
+  "number": "4.6",
+  "title": "Triangles and Quadrilaterals",
+  "body": " Triangles and Quadrilaterals   An introduction   "
+},
+{
+  "id": "sec-transformations-intro",
+  "level": "1",
+  "url": "sec-transformations-intro.html",
+  "type": "Section",
+  "number": "5.1",
+  "title": "Introduction to Geometric Transformations",
+  "body": " Introduction to Geometric Transformations   An introduction   "
+},
+{
+  "id": "sec-transformations-coord",
+  "level": "1",
+  "url": "sec-transformations-coord.html",
+  "type": "Section",
+  "number": "5.2",
+  "title": "Coordinate Mapping Rules",
+  "body": " Coordinate Mapping Rules   An introduction   "
+},
+{
+  "id": "sec-transformations-dilations",
+  "level": "1",
+  "url": "sec-transformations-dilations.html",
+  "type": "Section",
+  "number": "5.3",
+  "title": "Dilations",
+  "body": " Dilations   An introduction   "
+},
+{
+  "id": "sec-transformations-congruence",
+  "level": "1",
+  "url": "sec-transformations-congruence.html",
+  "type": "Section",
+  "number": "5.4",
+  "title": "Congruence",
+  "body": " Congruence   An introduction   "
+},
+{
+  "id": "sec-transformations-similarity",
+  "level": "1",
+  "url": "sec-transformations-similarity.html",
+  "type": "Section",
+  "number": "5.5",
+  "title": "Similarity",
+  "body": " Similarity   An introduction   "
+},
+{
+  "id": "sec-transformations-symmetry",
+  "level": "1",
+  "url": "sec-transformations-symmetry.html",
+  "type": "Section",
+  "number": "5.6",
+  "title": "Symmetry",
+  "body": " Symmetry   An introduction   "
+},
+{
+  "id": "sec-3d-intro",
+  "level": "1",
+  "url": "sec-3d-intro.html",
+  "type": "Section",
+  "number": "6.1",
+  "title": "Introduction to 3-Dimensional Shapes",
+  "body": " Introduction to 3-Dimensional Shapes   An introduction   "
+},
+{
+  "id": "sec-3d-platonic",
+  "level": "1",
+  "url": "sec-3d-platonic.html",
+  "type": "Section",
+  "number": "6.2",
+  "title": "Platonic Solids",
+  "body": " Platonic Solids   An introduction   "
+},
+{
+  "id": "sec-3d-prisms-pyramids",
+  "level": "1",
+  "url": "sec-3d-prisms-pyramids.html",
+  "type": "Section",
+  "number": "6.3",
+  "title": "Prisms and Pyramids",
+  "body": " Prisms and Pyramids   An introduction   "
+},
+{
+  "id": "sec-3d-nets",
+  "level": "1",
+  "url": "sec-3d-nets.html",
+  "type": "Section",
+  "number": "6.4",
+  "title": "Nets for 3-Dimensional Shapes",
+  "body": " Nets for 3-Dimensional Shapes   An introduction   "
+},
+{
+  "id": "sec-measurement-intro",
+  "level": "1",
+  "url": "sec-measurement-intro.html",
+  "type": "Section",
+  "number": "7.1",
+  "title": "Introduction to Measurements in Geometry",
+  "body": " Introduction to Measurements in Geometry   An introduction   "
+},
+{
+  "id": "sec-measurement-dist-angles",
+  "level": "1",
+  "url": "sec-measurement-dist-angles.html",
+  "type": "Section",
+  "number": "7.2",
+  "title": "Distances and Angles",
+  "body": " Distances and Angles   An introduction   "
+},
+{
+  "id": "sec-measurement-triangle-inequality",
+  "level": "1",
+  "url": "sec-measurement-triangle-inequality.html",
+  "type": "Section",
+  "number": "7.3",
+  "title": "The Triangle Inequality Theorem",
+  "body": " The Triangle Inequality Theorem   An introduction   "
+},
+{
+  "id": "sec-measurement-perimeter-area",
+  "level": "1",
+  "url": "sec-measurement-perimeter-area.html",
+  "type": "Section",
+  "number": "7.4",
+  "title": "Perimeter and Area",
+  "body": " Perimeter and Area   An introduction   "
+},
+{
+  "id": "sec-measurement-pythagorean",
+  "level": "1",
+  "url": "sec-measurement-pythagorean.html",
+  "type": "Section",
+  "number": "7.5",
+  "title": "The Pythagorean Theorem",
+  "body": " The Pythagorean Theorem   An introduction   "
+},
+{
+  "id": "sec-measurement-sa-vol",
+  "level": "1",
+  "url": "sec-measurement-sa-vol.html",
+  "type": "Section",
+  "number": "7.6",
+  "title": "Surface Area and Volume",
+  "body": " Surface Area and Volume   An introduction   "
+},
+{
+  "id": "sec-measurement-units",
+  "level": "1",
+  "url": "sec-measurement-units.html",
+  "type": "Section",
+  "number": "7.7",
+  "title": "Unit Conversions",
+  "body": " Unit Conversions   An introduction   "
 },
 {
   "id": "index-1",
